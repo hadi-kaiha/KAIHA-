9060120846f2bedf8bd312236d9abf8505f9a467
hadi-kaiha <megahubweb1@gmail.com>
2026-09-14 15:53:41 +0500

    Initialize Supabase client in supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+export const supabase = createClient(
+  'https://rlsmcomxugstueurdam.supabase.co',
+  'sb_publishable_mxCJKSppCAnMe6SwT7tbiQ_4dlOy122'
+)
