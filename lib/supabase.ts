c2bb6d57a8a5cb36d4257e687cc8bc1e34423469
hadi-kaiha <megahubweb1@gmail.com>
2026-09-15 14:06:48 +0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,1 @@
-import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(
-  'https://rlsmcomxugstueurdam.supabase.co',
-  'sb_publishable_mxCJKSppCAnMe6SwT7tbiQ_4dlOy122'
-)
+export const supabase = null
