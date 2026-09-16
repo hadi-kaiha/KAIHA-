10f55a0577b83e1218cc2b0dfcaa68d1a667b09b
hadi-kaiha <megahubweb1@gmail.com>
2026-09-16 18:54:57 +0500

    Remove wildcard hostname from remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: '**.unsplash.com' },
     ],
   },
 };
