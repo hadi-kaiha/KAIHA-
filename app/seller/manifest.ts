55b4b60537d2d096b7a6be4103ff679298090784
hadi-kaiha <megahubweb1@gmail.com>
2026-09-20 04:18:47 +0500

    Update manifest with description and new icons
    Added description and updated icons in the manifest.

--- app/seller/manifest.ts
+++ app/seller/manifest.ts
@@ -1,15 +1,27 @@
 import { MetadataRoute } from 'next'
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'KAIHA Seller',
     short_name: 'KAIHA Seller',
+    description: 'KAIHA Seller Panel',
+    id: '/seller',
     start_url: '/seller',
+    scope: '/seller',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',
     icons: [
-      { src: '/favicon.ico', sizes: 'any', type: 'image/x-icon' },
-      { src: '/icon.png', sizes: '512x512', type: 'image/png' }
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
     ],
   }
 }
