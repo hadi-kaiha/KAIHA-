8dbaedae2d43b3c5ff9dbd0f59842780fd4ecfff
hadi-kaiha <megahubweb1@gmail.com>
2026-09-20 04:30:58 +0500

    Modify manifest for KAIHA Rider
    Updated start_url to include source parameter and simplified icons array.

--- app/rider/manifest.ts
+++ app/rider/manifest.ts
@@ -1,27 +1,14 @@
 import { MetadataRoute } from 'next'
-
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'KAIHA Rider',
     short_name: 'KAIHA Rider',
-    description: 'KAIHA Rider Panel',
     id: '/rider',
-    start_url: '/rider',
+    start_url: '/rider?source=pwa',
     scope: '/rider',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',
-    icons: [
-      {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
+    icons: [{ src: '/icon-192x192.png', sizes: '192x192', type: 'image/png' }, { src: '/icon-512x512.png', sizes: '512x512', type: 'image/png' }],
   }
 }
